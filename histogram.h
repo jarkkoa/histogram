671d31e3aa405b7b0732b364dc7c60c7dfd76a0c
Jarkko Ahvenniemi <jarge98@gmail.com>
2023-03-05 17:07:30 +0200

    Update the documentation of class Histogram

--- histogram.h
+++ histogram.h
@@ -12,27 +12,36 @@ class Histogram : public Hist::HistogramBase
 
     public:
 
+        /**
+         * @brief Add an EInteger into the histogram
+         * 
+         */
         void add(Hist::EInteger) override;
 
         /**
          * @brief Get the mode of the histogram
-         * 
-         * @return Hist::EInteger 
+         * @pre At least one element is added to the histogram.
+         * @pre The distribution is unimodal
+         * @post Promises to return the mode of a unimodal histogram if the preconditions are satisfied
+         * @throws std::logic_error The histogram is empty.
+         * @return Hist::EInteger The mode of the histogram.
          */
         Hist::EInteger getMode() const override;
 
 
         /**
          * @brief Get the min value of the histogram
-         * 
-         * @return Hist::EInteger 
+         * @pre At least one element is added to the histogram
+         * @throws std::logic_error if the precondition isn't satisfied
+         * @return Hist::EInteger Min value of the histogram
          */
         Hist::EInteger getMinValue() const override;
 
 
         /**
          * @brief Get the max value of the histogram
-         * 
+         * @pre At least one element is added to the histogram
+         * @throws std::logic_error if the precondition isn't satisfied
          * @return Hist::EInteger 
          */
         Hist::EInteger getMaxValue() const override;
