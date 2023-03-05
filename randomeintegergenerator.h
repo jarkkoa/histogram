1351df9fac3089469c0d4e0b86f23fcd160e579d
Jarkko Ahvenniemi <jarge98@gmail.com>
2023-03-05 15:36:40 +0200

    Add declaration for the EInt generator class

---
+++ randomeintegergenerator.h
@@ -0,0 +1,14 @@
+#ifndef RANDOMEINTEGERGENERATOR_H
+
+#define RANDOMEINTEGERGENERATOR_H
+
+#include "histogrambase.h"
+
+class RandomEintegerGenerator : public Hist::RandomEintegerGeneratorBase
+{
+    public:
+
+        Hist::EInteger operator()() override;
+};
+
+#endif
